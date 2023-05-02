37d6b570e28f937083f6232cf474f2966853ca27
Talles C. Rodrigues <talles_correa@hotmail.com>
2023-05-02 11:57:43 -0300

    added new task

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ with connection:
 
     # Começo a manipular dados a partir daqui
 
+    # Inserindo um valor usando placeholder e um iterável
     with connection.cursor() as cursor:
         sql = (
             f'INSERT INTO {TABLE_NAME} '
@@ -50,6 +51,7 @@ with connection:
 
     connection.commit()
 
+    # Inserindo um valor usando placeholder e um dicionário
     with connection.cursor() as cursor:
         sql = (
             f'INSERT INTO {TABLE_NAME} '
@@ -67,6 +69,7 @@ with connection:
 
     connection.commit()
 
+    # Inserindo vários valores usando placeholder e um tupla de dicionários
     with connection.cursor() as cursor:
         sql = (
             f'INSERT INTO {TABLE_NAME} '
@@ -85,6 +88,7 @@ with connection:
         # print(result)
     connection.commit()
 
+    # Inserindo vários valores usando placeholder e um tupla de tuplas
     with connection.cursor() as cursor:
         sql = (
             f'INSERT INTO {TABLE_NAME} '
@@ -97,7 +101,18 @@ with connection:
             ("Helena", 15, ),
         )
         result = cursor.executemany(sql, data4)  # type:ignore
-        print(sql)
-        print(data4)
-        print(result)
+        # print(sql)
+        # print(data4)
+        # print(result)
     connection.commit()
+
+    # Lendo os valores com SELECT
+    with connection.cursor() as cursor:
+        sql = (
+            f'SELECT * FROM {TABLE_NAME} '
+        )
+        cursor.execute(sql)
+        data5 = cursor.fetchall()
+
+        for row in data5:
+            print(row)
