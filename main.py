e1d55fefc0b4d28c7ed7574e5cd291bb06e58b44
Talles C. Rodrigues <talles_correa@hotmail.com>
2023-05-03 13:32:02 -0300

    added new task

--- main.py
+++ main.py
@@ -109,15 +109,29 @@ with connection:
 
     # Lendo os valores com SELECT
     with connection.cursor() as cursor:
-        menor_id = int(input('Digite o menor id: '))
-        maior_id = int(input('Digite o maior id: '))
+        menor_id = 2
+        maior_id = 4
         sql = (
             f'SELECT * FROM {TABLE_NAME} '
             'WHERE id BETWEEN %s AND %s  '
         )
         cursor.execute(sql, (menor_id, maior_id))
-        print(cursor.mogrify(sql, (menor_id, maior_id)))
+        # print(cursor.mogrify(sql, (menor_id, maior_id)))
         data5 = cursor.fetchall()
 
-        for row in data5:
+        # for row in data5:
+        #     print(row)
+
+    # Apagando com DELETE, WHERE e placeholders no PyMySQL
+    with connection.cursor() as cursor:
+        sql = (
+            f'DELETE FROM {TABLE_NAME} '
+            'WHERE id = %s'
+        )
+        print(cursor.execute(sql, (2,)))
+        connection.commit()
+
+        cursor.execute(f'SELECT * FROM {TABLE_NAME} ')
+
+        for row in cursor.fetchall():
             print(row)
